bfa33bf5d4920b828b3620a46bd5c1f2cb23e40c
fensoft <fensoft@gmail.com>
2025-05-26 20:40:31 +0200

    enable i2c

--- build.sh
+++ build.sh
@@ -5,6 +5,8 @@ if [ ! -e rk3528-tvbox ]; then
   git clone https://github.com/ilyakurdyukov/rk3528-tvbox.git
 fi
 cd rk3528-tvbox
+git stash
+patch devicetree/orig/rk3528.dtsi < ../dt_i2c.patch
 cp ../uboot-new-gcc.patch armbian-patch/patch/u-boot/legacy/board_rk3528-tvbox
 if [ ! -e armbian-build ]; then
   git clone --depth=1 https://github.com/armbian/build armbian-build
@@ -15,6 +17,7 @@ mkdir -p userpatches/extensions
 # for i in ha docker-ce; do
 #   curl https://raw.githubusercontent.com/armbian/os/refs/heads/main/userpatches/extensions/$i.sh > userpatches/extensions/$i.sh
 # done
+# ENABLE_EXTENSIONS=ha
 ./compile.sh build BOARD=rk3528-tvbox BRANCH=legacy BUILD_DESKTOP=no BUILD_MINIMAL=yes EXPERT=yes KERNEL_CONFIGURE=no KERNEL_GIT=shallow RELEASE=bookworm
 cd ../..
 cat <<EOF > rk3528-tvbox/build.sh
